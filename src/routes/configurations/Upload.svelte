41405940baa46f89498890596c3540c7a8e84274
Douglas Ross <sachioross@gmail.com>
2022-05-12 15:55:14 -0400

    Updated Upload based on new load logic

--- src/routes/configurations/Upload.svelte
+++ src/routes/configurations/Upload.svelte
@@ -1,39 +1,40 @@
 <script>
-    import { onMount } from 'svelte';
     
     let files;
     let config = {};
 
-    onMount(async() => {
-        let loadedConfiguration = window.eapi.getConfig()?.configurationPath;
-        if (loadedConfiguration) {
+    const init = (async() => {
+        config = await window.eapi.getConfig();
+        if (config.configurationPath) {
             console.log("Loaded configuration");
-            files = [loadedConfiguration];
+            files = [config.configurationPath];
         }
+
+        return true
     })
+
+    let initialization = init();
 
     const setFileToUpload = () => {
         window.eapi.setUploadPath(files[0].name, files[0].path);
     }
-
-    const showConfigration = () => {
-        config = window.eapi.getConfig();
-        console.log(`The current configuration is: ${JSON.stringify(config, 2)}`);
-    }
 </script>
 
-<form>
-    <input type="file" id="myFile" name="filename" bind:files>
-    <button type="button" on:click|preventDefault={setFileToUpload}>Upload</button>    
-    <button type="button" on:click={showConfigration}>Get Config</button>
-    <div class="loaded-file">
-        <b>Currently loaded file:</b> {files ? files[0] : "No configuration specified"}
-    </div>
-    <pre>
-        {JSON.stringify(config, null, 2)}
-    </pre>
-</form> 
-
+{#await initialization}
+    <div>Loading...</div>
+{:then}
+    <form>
+        <input type="file" id="myFile" name="filename" bind:files>
+        <button type="button" on:click|preventDefault={setFileToUpload}>Upload</button>    
+        <!-- <button type="button" on:click={showConfigration}>Get Config</button> -->
+        <div class="loaded-file">
+            <b>Currently loaded file:</b> {files ? files[0] : "No configuration specified"}
+        </div>
+        <pre>
+            {JSON.stringify(config, null, 2)}
+        </pre>
+    </form> 
+{/await}
 <style>
     .loaded-file {
         padding: 1rem 0;
